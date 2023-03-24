b36f559af9d684decef2ed6cbd7031cbf4461570
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2023-03-24 15:06:29 +0100

    add routing group func

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -1,86 +1,71 @@
 <?php
+
 namespace app\Core;
 
 use app\Middleware\JWT;
 use Exception;
-
-class Router {
+class Router
+{
     private $routes = array();
+    private string $group_prefix = '';
 
-    public function get($route, $callback, $protected = false): void
+    public function get($route, $callback): void
     {
-        $this->addRoute('GET', $route, $callback, $protected);
+        $this->addRoute('GET', $this->group_prefix.$route, $callback);
     }
 
-    public function post($route, $callback, $protected = false): void
+    public function post($route, $callback): void
     {
-        $this->addRoute('POST', $route, $callback, $protected);
+        $this->addRoute('POST', $this->group_prefix.$route, $callback);
     }
 
-    public function put($route, $callback, $protected = false): void
+    public function put($route, $callback): void
     {
-        $this->addRoute('PUT', $route, $callback, $protected);
+        $this->addRoute('PUT', $this->group_prefix.$route, $callback);
     }
 
-    public function delete($route, $callback, $protected = false): void
+    public function delete($route, $callback): void
     {
-        $this->addRoute('DELETE', $route, $callback, $protected);
+        $this->addRoute('DELETE', $this->group_prefix.$route, $callback);
     }
 
+    public function group($prefix, $callback) {
+        $previous_group_prefix = $this->group_prefix;
+        $this->group_prefix .= $prefix;
+        $callback($this);
+        $this->group_prefix = $previous_group_prefix;
+    }
 
-    private function addRoute($method, $route, $callback, $protected) {
+    private function addRoute($method, $route, $callback)
+    {
         $route = preg_replace('/\{(\w+)\}/', '(\w+)', $route);
         $route = "#^$route$#";
-        $this->routes[$method][$route] = array('callback' => $callback, 'protected' => $protected);
+        $this->routes[$method][$route] = array('callback' => $callback);
     }
 
-    public function route() {
+    public function route()
+    {
+        /*
+        Replace it, if the project is in sub directory
+        $uri_a = substr($uri, strpos($uri, "/") +1);
+        $uri = substr($uri_a, strpos($uri_a, "/") +1);
+         */
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
-    
-        if (array_key_exists($method, $this->routes)) { 
+        if (array_key_exists($method, $this->routes)) {
             foreach ($this->routes[$method] as $route => $data) {
                 if (preg_match($route, $uri, $matches)) {
-                    if ($data['protected']) {
-                        $headers = apache_request_headers();
-                        
-                        if (!isset($headers['Authorization'])) {
-                            header("HTTP/1.0 401");
-                            $array['response'] = "Token not sent.";
-                            echo json_encode($array);
-                            return;
-                        } else {
-                            $token = explode(' ', $headers['Authorization'])[1];
-                            try {
-                                $jwt = new JWT(trim(getenv('TOKEN_KEY')));
-                                $decoded = $jwt->decode($token);
-                                if($decoded != true){
-                                   throw new Exception($decoded);
-                                }
-                                array_shift($matches);
-                                $callback = $data['callback'];
-                                $class = new $callback[0];
-                                call_user_func_array([$class, $callback[1]], $matches);
-                                return;
-                            } catch (Exception $e) {
-                                header("HTTP/1.0 401");
-                                $array['response'] = $e->getMessage();
-                                echo json_encode($array);
-                                return;
-                            }
-                        }
-                    } else {
-                        array_shift($matches);
-                        $callback = $data['callback']; 
-                        $class = new $callback[0];
-                        call_user_func_array([$class, $callback[1]], $matches);
-                        return;
+                    array_shift($matches);
+                    $callback = $data['callback'];
+                    if (is_callable($callback)) {
+                        call_user_func_array($callback, $matches);
+                        exit();
                     }
+                    $class = new $callback[0];
+                    call_user_func_array([$class, $callback[1]], $matches);
+                    return;
                 }
             }
-            header("HTTP/1.0 404");
-            $array['response'] = "Not found.";
-            echo json_encode($array);
         } else {
             header("HTTP/1.0 405");
             $array['response'] = "Method not allowed.";
